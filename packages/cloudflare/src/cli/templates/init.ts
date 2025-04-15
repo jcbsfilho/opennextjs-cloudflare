f8ebbfc71fd3cca8df947bc3b6650d4708e9b769
José Filho <jc.bsfilho@gmail.com>
2025-04-15 13:09:28 -0300

    fix: remove commented-out process version assignments in initRuntime

--- packages/cloudflare/src/cli/templates/init.ts
+++ packages/cloudflare/src/cli/templates/init.ts
@@ -55,9 +55,8 @@ export function init(_request: Request, _env: CloudflareEnv) {
 function initRuntime() {
   // Some packages rely on `process.version` and `process.versions.node` (i.e. Jose@4)
   // TODO: Remove when https://github.com/unjs/unenv/pull/493 is merged
-  Object.assign(process, { version: process.version || "v22.14.0" });
-  // @ts-expect-error Node type does not match workerd
-  Object.assign(process.versions, { node: "22.14.0", ...process.versions });
+  // Object.assign(process, { version: process.version || "v22.14.0" });
+  // Object.assign(process.versions, { node: "22.14.0", ...process.versions });
 
   globalThis.__dirname ??= "";
   globalThis.__filename ??= "";

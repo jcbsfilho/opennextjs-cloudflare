f489940b6e4361fa1c6bd07a9afc5d7be1b7f614
José Filho <jc.bsfilho@gmail.com>
2025-04-24 18:30:36 -0300

    fix: enhance require to import transformation for specific Node.js modules

--- packages/cloudflare/src/cli/build/patches/plugins/require.ts
+++ packages/cloudflare/src/cli/build/patches/plugins/require.ts
@@ -44,6 +44,16 @@ export function fixRequire(updater: ContentUpdater): Plugin {
             `require("next/dist/compiled/@opentelemetry/api")`
           );
 
+          // replace any require to import
+          contents = contents.replace(/require\((["'])(.+?)\1\)/g, (_, __, path) => {
+            // Se o caminho for um módulo Node.js ou de node_modules, use import dinâmico
+            const pathsMatch = ["node:stream", "stream"];
+            if (pathsMatch.includes(path)) {
+              return `globalThis.stream`;
+            }
+            return _;
+          });
+
           return contents;
         },
       },
